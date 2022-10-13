69e88bbefbcb3d188393df222df145e0232e84a7
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2022-10-14 00:19:52 +0430

    Add helpers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'recipes/index', to: 'recipes#public'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :users
-  resources :foods 
+  root "recipes#public"
+  resources :users do
+    resources :recipes
+  end
+  resources :foods
+  resources :recipes
 end
