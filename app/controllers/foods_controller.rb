44b8448826f6bdd921e685c8fb1e563717a63689
shadabwahidullah <shadabwahidullah@gmail.com>
2022-10-13 21:43:52 +0430

    Add remove button for foods

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -22,4 +22,9 @@ class FoodsController < ApplicationController
       end
     end
   end
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+    redirect_to foods_path
+  end
 end
