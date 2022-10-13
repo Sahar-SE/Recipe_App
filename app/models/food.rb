f2c6cedde34b79fc945cb0ef9badce9f6b1670fe
shadabwahidullah <shadabwahidullah@gmail.com>
2022-10-13 21:29:13 +0430

    Add form for new food

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,9 @@
 class Food < ApplicationRecord
-  belongs_to :users
-  has_many :receipFoods
+  belongs_to :user
+  # has_many :receipFoods
+
+  validates :name, presence: true, length: {in: 1..255}
+  validates :measurement_unit, presence: true, length: {in: 1..255}
+  validates :price, numericality: {decimal: true, greater_than: 0}
+  validates :quantity, numericality: {integer: true, greater_than: 0}
 end
