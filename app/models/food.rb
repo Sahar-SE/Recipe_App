1982ea33b14ac0bc059d218f245d4b3fd74b93ae
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2022-10-14 00:13:40 +0430

    Add food  model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,6 @@
 class Food < ApplicationRecord
   belongs_to :users
-  has_many :receipFoods
+  has_many :recipes_foods, dependent: :destroy
+
+  validates :name, :price, :quantity, :measurement_unit, presence: true
 end
