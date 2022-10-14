81130939840610344220d0e167ace18c6e7ac99e
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2022-10-14 16:13:45 +0430

    Solve N+1 queries

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,14 +1,35 @@
 class RecipesController < ApplicationController
   def index
-    @recipes = Recipe.where(user_id: current_user.id)
+    @recipes = Recipe.includes([:user]).all.order(created_at: :desc)
   end
 
   def public
-    @public_recipes = Recipe.where(public: true)
+    @public_recipes = Recipe.includes([:user]).where(public: true).order(created_at: :desc)
   end
 
   def show
-    @recipe = Recipe.find(params[:id])
+    @recipe = Recipe.includes([:user]).find(params[:id])
+  end
+
+  def new
+    @user = current_user
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user_id = current_user.id
+    if @recipe.save
+      redirect_to user_recipes_path
+    else
+      render 'show'
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
   end
 
   def destroy
