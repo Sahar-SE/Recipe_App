78473fba90d5909933e3057276386b63ff3ce9c5
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2022-10-14 16:47:02 +0430

    Fix Linters

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -40,6 +40,5 @@ class RecipesController < ApplicationController
         redirect_to user_recipes_path(user_id: @recipe.user.id), notice: 'Recipe was successfully deleted.'
       end
     end
-
   end
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,4 @@
 class Food < ApplicationRecord
-
   belongs_to :users
   has_many :recipes_foods, dependent: :destroy
 
